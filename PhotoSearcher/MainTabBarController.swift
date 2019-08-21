a55e115da3140b423dcd18389b03032a54406b09
Alexander Omelchuk <cattivole@icloud.com>
2019-08-21 13:32:39 +0300

    TabBar Create

---
+++ PhotoSearcher/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  PhotoSearcher
+//
+//  Created by Александр Омельчук on 21.08.2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+    }
+}
