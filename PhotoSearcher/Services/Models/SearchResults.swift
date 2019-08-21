00b4dc3828f9191673db61a1d144845971b0ceeb
Alexander Omelchuk <cattivole@icloud.com>
2019-08-21 16:56:19 +0300

    SearchResults

---
+++ PhotoSearcher/Services/Models/SearchResults.swift
@@ -0,0 +1,30 @@
+//
+//  SearchResults.swift
+//  PhotoSearcher
+//
+//  Created by Александр Омельчук on 21.08.2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResults: Decodable {
+    
+    let total: Int
+    let results: [UnsplashPhoto]
+    
+}
+
+struct UnsplashPhoto: Decodable {
+    let width: Int
+    let height: Int
+    let urls: [URLKing.RawValue: String]
+    
+    enum URLKing: String {
+        case raw
+        case full
+        case regular
+        case small
+        case thumb
+    }
+}
---
+++ PhotoSearcher/Services/NetworkDataFetcher.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkDataFetcher.swift
+//  PhotoSearcher
+//
+//  Created by Александр Омельчук on 21.08.2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import Foundation
+
+class NetworkDataFetcher {
+    
+    //func fetchImages(searchTerm: String, completion) {
+        
+    //}
+    
+    
+    
+}
