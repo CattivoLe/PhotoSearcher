dc763297a6bd9c5bfca5609e2999d9f15b7b82c8
Alexander Omelchuk <cattivole@icloud.com>
2019-08-21 13:26:57 +0300

    Initial Commit

---
+++ PhotoSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoSearcher
+//
+//  Created by Александр Омельчук on 21.08.2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
