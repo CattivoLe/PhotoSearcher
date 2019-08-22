d12a7041890aff57b50db58fe7e11774568414f1
Alexander Omelchuk <cattivole@icloud.com>
2019-08-22 12:40:44 +0300

    Cell, imageView

---
+++ PhotoSearcher/PhotosCell.swift
@@ -0,0 +1,56 @@
+//
+//  PhotosCell.swift
+//  PhotoSearcher
+//
+//  Created by Александр Омельчук on 22.08.2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCell: UICollectionViewCell {
+    
+    static let reuseId = "PhotosCell"
+    
+    private let checkMark: UIImageView = {
+        let image = UIImage(named: "bird")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
+        return imageView
+    }()
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        setupPhotoImageView()
+        setupCheckMarkView()
+    }
+    
+    private func setupPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+    }
+    
+    private func setupCheckMarkView() {
+        addSubview(checkMark)
+        checkMark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
+        checkMark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
