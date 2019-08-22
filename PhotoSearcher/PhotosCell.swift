9f6578069391300959248d673b04de02f63293a1
Alexander Omelchuk <cattivole@icloud.com>
2019-08-22 12:47:16 +0300

    Photo Cell

--- PhotoSearcher/PhotosCell.swift
+++ PhotoSearcher/PhotosCell.swift
@@ -28,10 +28,22 @@ class PhotosCell: UICollectionViewCell {
         return imageView
     }()
     
+    override var isSelected: Bool {
+        didSet {
+            updateSelectedState()
+        }
+    }
+    
+    override func prepareForReuse() {
+        super .prepareForReuse()
+        photoImageView.image = nil
+    }
+    
     override init(frame: CGRect) {
         super .init(frame: frame)
         setupPhotoImageView()
         setupCheckMarkView()
+        updateSelectedState()
     }
     
     private func setupPhotoImageView() {
@@ -46,6 +58,11 @@ class PhotosCell: UICollectionViewCell {
         addSubview(checkMark)
         checkMark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
         checkMark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
+    }
+    
+    private func updateSelectedState() {
+        photoImageView.alpha = isSelected ? 0.7 : 1
+        checkMark.alpha = isSelected ? 1 : 0
     }
     
     required init?(coder aDecoder: NSCoder) {
